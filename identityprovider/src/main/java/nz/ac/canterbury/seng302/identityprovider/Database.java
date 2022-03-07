a48214fb4e94d479f9a451f3d04672d9382d7bad
Jamie Thomas <jth141@uclive.ac.nz>
2022-03-08 11:55:26 +1300

    Added the login screen to use the database

--- identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/Database.java
+++ identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/Database.java
@@ -6,81 +6,93 @@ import java.util.Arrays;
 
 public class Database {
 
-    /* Hardcoded users */
-    public static ArrayList<ArrayList<String>> database = new ArrayList<>() {
-        {
-            add(new ArrayList<>() {
-                {
-                    add("admin");
-                    add("admin");
-                }
-            });
-            add(new ArrayList<>() {
-                {
-                    add("student");
-                    add("student");
-                }
-            });
-            add(new ArrayList<>() {
-                {
-                    add("teacher");
-                    add("teacher");
-                }
-            });
-        }
-    };
+    private Connection conn;
 
     public Database() {
-        Connection conn = null;
         try {
-            conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
-            System.out.println("Connected to database");
-            // Creates the table
-            //conn.prepareStatement("CREATE TABLE userTable (Id int NOT NULL, Username VARCHAR(30) NOT NULL, Password VARCHAR(30) NOT NULL);").execute();
-            // Hardcode a user
-            //conn.createStatement().execute("INSERT INTO userTable VALUES (1, 'database', 'database');");
-            //System.out.println("Inserted");
+            conn = connectToDatabase();
+            if (conn != null) {
+                System.out.println("Connected to database");
 
+                //conn.prepareStatement("DROP TABLE IF EXISTS userTable;").execute();
+                //conn.createStatement().execute("INSERT INTO userTable VALUES (1, 'database1', 'database1p');");
 
+                // Check table is built
+                try {
+                    conn.prepareStatement("CREATE TABLE userTable (Id int NOT NULL UNIQUE PRIMARY KEY, Username VARCHAR(30) NOT NULL, Password VARCHAR(30) NOT NULL);").execute();
+                } catch (SQLException ignored) {}
 
-            //Currently running
-            conn.prepareStatement("DROP TABLE IF EXISTS userTable;").execute();
-            conn.prepareStatement("CREATE TABLE userTable (Id int NOT NULL, Username VARCHAR(30) NOT NULL, Password VARCHAR(30) NOT NULL);").execute();
+                // For testing
+                System.out.println(this);
 
-            //Insert some values
-            conn.createStatement().execute("INSERT INTO userTable VALUES (1, 'database1', 'database1p');");
-            conn.createStatement().execute("INSERT INTO userTable VALUES (2, 'database2', 'database2p');");
-            conn.createStatement().execute("INSERT INTO userTable VALUES (3, 'database3', 'database3p');");
-
-            //Get data back
-            ResultSet allTable = conn.createStatement().executeQuery("SELECT * FROM userTable");
-            System.out.println(allTable);
-
-            //Iterate through ResultSet to get data
-            while (allTable.next()) {
-                System.out.println("ID: " + allTable.getString("ID") + " Username: " + allTable.getString("Username") + " Password: " + allTable.getString("password"));
+                conn.close();
             }
-
-            System.out.println("Line Run");
-
-            conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Connects to database and handles errors.
+     * @return Connection object used to make statements to
+     */
+    private Connection connectToDatabase() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to database. ");
+        }
+        return conn;
+    }
 
-        /**
+    /**
+     * Returns the entire database saved.
+     * @return String holding all items in database
+     */
+    public String toString() {
+        //Get data back
+        StringBuilder databaseString = new StringBuilder();
+        try {
+            conn = connectToDatabase();
+            if (conn != null) {
+                ResultSet allTable = conn.createStatement().executeQuery("SELECT * FROM userTable");
+                //Iterate through ResultSet to get data
+                while (allTable.next()) {
+                    databaseString
+                            .append("ID: ").append(allTable.getString("ID"))
+                            .append("  Username: ").append(allTable.getString("Username"))
+                            .append("  Password: ").append(allTable.getString("password"))
+                            .append("\n");
+                }
+                conn.close();
+            }
+
+        } catch (SQLException ignored) {}
+
+        return databaseString.toString();
+    }
+
+
+
+    /**
      * Checks if the user is in the database and has the right password.
      * @return boolean of if the user is in the database
      */
     public boolean inDatabase(String username, String password) {
         boolean isInDatabase = false;
-        for (ArrayList<String> users : database) {
-            if (username.equals(users.get(0)) && password.equals(users.get(1))) {
-                isInDatabase = true;
-                break;
+        conn = connectToDatabase();
+        if (conn != null) {
+            try {
+                String sqlStatement = "SELECT * FROM userTable WHERE username='" + username + "' AND password='" + password + "'";
+                ResultSet resultingAccount = conn.createStatement().executeQuery(sqlStatement);
+                if (resultingAccount.next()) {
+                    isInDatabase = true;
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
+
         }
         return isInDatabase;
     }
