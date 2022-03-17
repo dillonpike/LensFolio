f40ed208f10ecbc8b21581afbe4738db0b07301c
Haipeng Liu <hli165@uclive.ac.nz>
2022-03-17 19:29:26 +1300

    Implemented service/controller for edit account page, added method to request edit data. #feature

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/EditAccountController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/EditAccountController.java
@@ -4,12 +4,18 @@ import nz.ac.canterbury.seng302.portfolio.service.AuthenticateClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import nz.ac.canterbury.seng302.portfolio.service.EditAccountServerService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class EditAccountController {
 
     @Autowired
-    private AuthenticateClientService authenticateClientService;
+    private EditAccountServerService editAccountServerService;
 
     /***
      * Generate the edit account page which let user edit info/attributes
@@ -20,5 +26,18 @@ public class EditAccountController {
     public String showEditAccountPage(
     ) {
        return "editAccount";
+    }
+
+    @PostMapping("/editAccount")
+    public String editAccount(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @RequestParam(name = "newFullName") String newFullName,
+            @RequestParam(name = "newNickName") String newNickName,
+            @RequestParam(name = "newEmail") String newEmail,
+            @RequestParam(name = "newGender") String newGender,
+            @RequestParam(name = "newBio") String newBio
+    ) {
+        return "editAccount";
     }
 }
