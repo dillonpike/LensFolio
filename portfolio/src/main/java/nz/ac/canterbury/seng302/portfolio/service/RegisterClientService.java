210ac181fb07a660adc606d17391001502b556be
Haipeng Liu <hli165@uclive.ac.nz>
2022-03-18 14:47:20 +1300

    Added methods encryptPassword to encrypt password when registering.

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/RegisterClientService.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/RegisterClientService.java
@@ -5,6 +5,7 @@ import nz.ac.canterbury.seng302.shared.identityprovider.GreeterGrpc;
 import nz.ac.canterbury.seng302.shared.identityprovider.UserAccountServiceGrpc;
 import nz.ac.canterbury.seng302.shared.identityprovider.UserRegisterRequest;
 import nz.ac.canterbury.seng302.shared.identityprovider.UserRegisterResponse;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,14 +15,22 @@ public class RegisterClientService {
     private UserAccountServiceGrpc.UserAccountServiceBlockingStub UserAccountStub;
 
     public UserRegisterResponse receiveConformation(final String username, final String password, final String fullName, final String email) {
+        String cryptedPassword = encryptPassword(password);
         UserRegisterRequest response = UserRegisterRequest.newBuilder()
                 .setUsername(username)
-                .setPassword(password)
+                .setPassword(cryptedPassword)
                 .setFirstName(fullName)
                 .setLastName("")
                 .setEmail(email)
                 .build();
         return UserAccountStub.register(response);
+    }
+
+    public String encryptPassword (String password) {
+        Pbkdf2PasswordEncoder pbkdf2PasswordEncoder = new Pbkdf2PasswordEncoder();
+        String cryptedPassword = pbkdf2PasswordEncoder.encode(password);
+//        boolean passwordIsValid = pbkdf2PasswordEncoder.matches("lhp20010308", pbkdf2CryptedPassword);
+        return cryptedPassword;
     }
 
 }
