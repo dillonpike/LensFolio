4f1470c4018bd38f7e39de1fec1f9cf839021985
Christopher Hamdajani <ckh35@uclive.ac.nz>
2022-03-20 22:29:01 +1300

    display user's information to editAccount page and fix button navigation error #feature #fix

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/AccountController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/AccountController.java
@@ -7,9 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 @Controller
@@ -42,16 +46,23 @@ public class AccountController {
             model.addAttribute("bio", getUserByIdReply.getBio());
             String fullName = getUserByIdReply.getFirstName() + " " + getUserByIdReply.getMiddleName() + " " + getUserByIdReply.getLastName();
             model.addAttribute("fullName", fullName);
+            model.addAttribute("userId", userId);
         } catch (StatusRuntimeException e) {
             model.addAttribute("loginMessage", "Error connecting to Identity Provider...");
             e.printStackTrace();
         }
-
-        // Giving the model the new variables
-        //model.addAttribute("firstName", getUserByIdReply.getFirstName());
-        //model.addAttribute("lastName", getUserByIdReply.getLastName());
-        //model.addAttribute("nickname", getUserByIdReply.getNickname());
-
         return "account";
+    }
+
+    @PostMapping("/backToAccountPage")
+    public String editAccount(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @ModelAttribute("userId") int userId,
+            RedirectAttributes rm
+    ) {
+        System.out.println("enter load edit account");
+        rm.addAttribute("userId",userId);
+        return "redirect:account";
     }
 }
--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/EditAccountController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/EditAccountController.java
@@ -1,12 +1,19 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
+import io.grpc.StatusRuntimeException;
+import nz.ac.canterbury.seng302.portfolio.model.Sprint;
+import nz.ac.canterbury.seng302.portfolio.service.RegisterClientService;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserRegisterRequestOrBuilder;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import nz.ac.canterbury.seng302.portfolio.service.EditAccountClientService;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,6 +24,9 @@ public class EditAccountController {
     @Autowired
     private EditAccountClientService editAccountClientService;
 
+    @Autowired
+    private RegisterClientService registerClientService;
+
     /***
      * Generate the edit account page which let user edit info/attributes
      *
@@ -24,22 +34,37 @@ public class EditAccountController {
      */
     @GetMapping("/editAccount")
     public String showEditAccountPage(
+            @RequestParam(value = "userId") int userId, Model model
     ) {
+        UserResponse getUserByIdReply;
+        try {
+            getUserByIdReply = registerClientService.getUserData(userId);
+            model.addAttribute("firstName", getUserByIdReply.getFirstName());
+            model.addAttribute("lastName", getUserByIdReply.getLastName());
+            model.addAttribute("username", getUserByIdReply.getUsername());
+            model.addAttribute("middleName", getUserByIdReply.getMiddleName());
+            model.addAttribute("email", getUserByIdReply.getEmail());
+            model.addAttribute("personalPronouns", getUserByIdReply.getPersonalPronouns());
+            model.addAttribute("bio", getUserByIdReply.getBio());
+            String fullName = getUserByIdReply.getFirstName() + " " + getUserByIdReply.getMiddleName() + " " + getUserByIdReply.getLastName();
+            model.addAttribute("fullName", fullName);
+            model.addAttribute("userId", userId);
+        } catch (StatusRuntimeException e) {
+            model.addAttribute("loginMessage", "Error connecting to Identity Provider...");
+            e.printStackTrace();
+        }
        return "editAccount";
     }
 
-    @PostMapping("/editAccount")
+    @PostMapping("/editAccountLoad")
     public String editAccount(
             HttpServletRequest request,
             HttpServletResponse response,
-            @RequestParam(name = "userId") String userId,
-            @RequestParam(name = "newFullName") String newFullName,
-            @RequestParam(name = "newNickName") String newNickName,
-            @RequestParam(name = "newEmail") String newEmail,
-            @RequestParam(name = "newGender") String newGender,
-            @RequestParam(name = "newBio") String newBio,
-            Model model
+            @ModelAttribute("userId") int userId,
+            RedirectAttributes rm
     ) {
-        return "editAccount";
+        System.out.println("enter load edit account");
+        rm.addAttribute("userId",userId);
+        return "redirect:editAccount";
     }
 }
