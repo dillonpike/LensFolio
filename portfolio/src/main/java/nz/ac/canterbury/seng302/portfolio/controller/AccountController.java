e674ce67cee2610ae2bed5b4e31cb87f36486480
Jamie Thomas <jth141@uclive.ac.nz>
2022-03-22 18:53:25 +1300

    Added "Years since, months since" times for how long the user has existed and have it displaying on the account page.

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/AccountController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/AccountController.java
@@ -17,6 +17,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.Date;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.TemporalUnit;
 import java.util.Map;
 
 @Controller
@@ -51,6 +55,7 @@ public class AccountController {
             model.addAttribute("fullName", fullName);
             model.addAttribute("userId", userId);
             model.addAttribute("dateAdded", getDateAddedString(getUserByIdReply.getCreated()));
+            model.addAttribute("monthsSinceAdded", getDateSinceAddedString(getUserByIdReply.getCreated()));
         } catch (StatusRuntimeException e) {
             model.addAttribute("loginMessage", "Error connecting to Identity Provider...");
             e.printStackTrace();
@@ -65,6 +70,56 @@ public class AccountController {
             return dateFormat.format(date);
         } else {
             return null;
+        }
+    }
+
+    private String getDateSinceAddedString(Timestamp dateAdded) {
+        StringBuilder timeSinceAddedSB = new StringBuilder();
+        timeSinceAddedSB.append("(");
+
+        int yearsSince = getYearsSinceAdded(dateAdded);
+        if (yearsSince > 0) {
+            timeSinceAddedSB.append(yearsSince);
+            timeSinceAddedSB.append(" Year");
+            if (yearsSince == 1) {
+                timeSinceAddedSB.append(", ");
+            } else {
+                timeSinceAddedSB.append("s, ");
+            }
+        }
+        int monthsSince = getMonthsSinceAdded(dateAdded);
+        timeSinceAddedSB.append(monthsSince % 12);
+        timeSinceAddedSB.append(" Month");
+        if (monthsSince % 12 == 1) {
+            timeSinceAddedSB.append(")");
+        } else {
+            timeSinceAddedSB.append("s)");
+        }
+
+        return timeSinceAddedSB.toString();
+    }
+
+    private int getMonthsSinceAdded(Timestamp dateAdded) {
+        if (dateAdded != null) {
+            Period difference = Period.between(
+                    LocalDate.ofEpochDay((dateAdded.getSeconds() / 86400)),
+                    LocalDate.now()
+            );
+            return difference.getMonths();
+        } else {
+            return 0;
+        }
+    }
+
+    private int getYearsSinceAdded(Timestamp dateAdded) {
+        if (dateAdded != null) {
+            Period difference = Period.between(
+                    LocalDate.ofEpochDay((dateAdded.getSeconds() / 86400)),
+                    LocalDate.now()
+            );
+            return difference.getYears();
+        } else {
+            return 0;
         }
     }
 
