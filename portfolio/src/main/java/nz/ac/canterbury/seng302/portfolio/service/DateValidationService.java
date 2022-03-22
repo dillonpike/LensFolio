b978a3affc3137f922f73d6e8fab69c5d31cc178
Rachel Hodgson <rlh89@uclive.ac.nz>
2022-03-22 19:32:21 +1300

    Added error messages for invalid dates like 29th, 30th and 31st of February as well as the 31st of April, June, September and November #feature

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/DateValidationService.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/DateValidationService.java
@@ -1,5 +1,8 @@
 package nz.ac.canterbury.seng302.portfolio.service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
 import nz.ac.canterbury.seng302.portfolio.model.Project;
 import nz.ac.canterbury.seng302.portfolio.model.Sprint;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,12 +47,62 @@ public class DateValidationService {
     }
 
     public String validateProjectStartDate(String startDateString, String endDateString) {
+        // Checks if the start date is before the end date
         String message = "";
         Date startDate = Project.stringToDate(startDateString);
         Date endDate = Project.stringToDate(endDateString);
         if (startDate.after(endDate)) {
             message =  "Start date must be on or before the end date.";
         }
+
+        // Check the start date is less than a year ago
+        long diff = getDiff(startDate);
+        if (diff >= 365) {
+            message = "Start date must be less than a year ago";
+        }
+
+        String startDateIsValid = isDateValid(startDateString);
+        String endDateIsValid = isDateValid(endDateString);
+
+        if (! startDateIsValid.equals("")) {
+            message = startDateIsValid;
+        }
+
+        if (! endDateIsValid.equals("")) {
+            message = endDateIsValid;
+        }
+
+        return message;
+    }
+
+    public long getDiff(Date startDate){
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MMM/yyyy");
+        String formatDateTime = now.format(formatter);
+        Date today = Project.stringToDate(formatDateTime);
+
+        long diffInMs = Math.abs(today.getTime() - startDate.getTime());
+
+        return TimeUnit.DAYS.convert(diffInMs, TimeUnit.MILLISECONDS);
+    }
+
+    public String isDateValid(String date) {
+        String message = "";
+        int day = Integer.parseInt(date.substring(0, 2));
+        String month = date.substring(3, 6);
+        int year = Integer.parseInt(date.substring(7, 11));
+
+
+        if ("Feb".equals(month) && (day == 30 || day == 31)) {
+            message = "This is an invalid date for February";
+        }
+        if (year % 4 != 0 && "Feb".equals(month) && day == 29) {
+            message = String.format("This is an invalid date for February in the year %d", year);
+        }
+        if (day == 31 && ("Apr".equals(month) || "Jun".equals(month) || "Sep".equals(month) || "Nov".equals(month))) {
+            message = "There is no 31st of the chosen month";
+        }
+
         return message;
     }
 }
