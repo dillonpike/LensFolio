a3a9f08048eff711c02202d2435ebf663887613a
Wil <wjo34@uclive.ac.nz>
2022-03-27 16:45:11 +1300

    Added the account information panel content on the side of the edit password page #feature

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/editPasswordController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/editPasswordController.java
@@ -1,15 +1,34 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
+import io.grpc.StatusRuntimeException;
 import nz.ac.canterbury.seng302.portfolio.service.AuthenticateClientService;
+import nz.ac.canterbury.seng302.portfolio.service.RegisterClientService;
+import nz.ac.canterbury.seng302.portfolio.service.UserAccountService;
+import nz.ac.canterbury.seng302.portfolio.utility.Utility;
+import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class editPasswordController {
 
     @Autowired
     private AuthenticateClientService authenticateClientService;
+
+    @Autowired
+    private RegisterClientService registerClientService;
+
+    @Autowired
+    private UserAccountService userAccountService;
+
+    private Utility utility = new Utility();
 
     /***
      * GET Method
@@ -20,7 +39,26 @@ public class editPasswordController {
      */
     @GetMapping("/editPassword")
     public String showEditPassword(
+            Model model,
+            HttpServletRequest request,
+            @RequestParam(value = "userId") int userId,
+            @AuthenticationPrincipal AuthState principal
     ) {
+        Integer id = userAccountService.getUserIDFromAuthState(principal);
+        try {
+            UserResponse getUserByIdReply = registerClientService.getUserData(id);
+            String fullName = getUserByIdReply.getFirstName() + " " + getUserByIdReply.getMiddleName() + " " + getUserByIdReply.getLastName();
+            model.addAttribute("fullName", fullName);
+            model.addAttribute("username", getUserByIdReply.getUsername());
+            model.addAttribute("dateAdded", utility.getDateAddedString(getUserByIdReply.getCreated()));
+            model.addAttribute("monthsSinceAdded", utility.getDateSinceAddedString(getUserByIdReply.getCreated()));
+        } catch(Exception e) {
+            model.addAttribute("loginMessage", "Error connecting to Identity Provider...");
+            e.printStackTrace();
+        }
+
+
+
         return "editPassword";
     }
 }
