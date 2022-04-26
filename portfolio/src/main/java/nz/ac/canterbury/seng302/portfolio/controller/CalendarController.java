e70df03a0adeb76f6d8f89ed025b3cdc01711b7b
Wil <wjo34@uclive.ac.nz>
2022-04-26 15:38:20 +1200

    Greyed out the dates from the projects start date to its end date #feature #pair[jel119, wjo34]

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/CalendarController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/CalendarController.java
@@ -1,11 +1,25 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
+import com.google.protobuf.Timestamp;
+import nz.ac.canterbury.seng302.portfolio.model.Project;
+import nz.ac.canterbury.seng302.portfolio.model.Sprint;
+import nz.ac.canterbury.seng302.portfolio.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
+
 @Controller
 public class CalendarController {
+
+    @Autowired
+    private ProjectService projectService;
 
     /***
      * GET request method, followed by the request URL(../calendar)
@@ -16,7 +30,15 @@ public class CalendarController {
      */
     @GetMapping("/calendar")
     public String calendarPage(Model model) throws Exception {
-
+        Project project;
+        try {
+            project = projectService.getProjectById(0);
+        } catch (Exception e) {
+            return "404NotFound";
+        }
+        Date endDate = SprintLifetimeController.getUpdatedDate(project.getEndDate(), 1, 0);
+        model.addAttribute("startDate", project.getStartDate());
+        model.addAttribute("endDate", endDate);
         return "calendar";
 }
 
