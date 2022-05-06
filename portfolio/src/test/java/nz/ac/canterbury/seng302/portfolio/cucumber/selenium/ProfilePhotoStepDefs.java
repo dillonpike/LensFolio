b354841dfb1569c8de62793a270c781dec3060c4
Dillon Pike <dkp33@uclive.ac.nz>
2022-05-06 17:11:26 +1200

    Fixed profile photo tests. Had an issue that the photo displayed on the account page was lower resolution than what was stored. Refactored duplicate photo checking code to a method. #fix #refactor

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/cucumber/selenium/ProfilePhotoStepDefs.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/cucumber/selenium/ProfilePhotoStepDefs.java
@@ -18,9 +18,9 @@ import ru.yandex.qatools.ashot.comparison.ImageDiff;
 import ru.yandex.qatools.ashot.comparison.ImageDiffer;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -64,59 +64,83 @@ public class ProfilePhotoStepDefs {
         wait.until(ExpectedConditions.elementToBeClickable(By.id("removeUpdateAlert")));
     }
 
-    @Then("My profile photo is the default photo")
-    public void myProfilePhotoIsTheDefaultPhoto() {
-        assertTrue(webDriver.findElement(By.id("uploadPreview")).isDisplayed());
-
-        WebElement profilePhotoElement = webDriver.findElement(By.id("uploadPreview"));
-
+    /**
+     * Returns true if the user's profile photo is the same as the image stored at imagePath, otherwise false.
+     * @param profilePhotoElement WebElement that displays the user's profile photo
+     * @param imagePath image to check the user's profile photo is the same as
+     * @return true if the profile photo is the same, otherwise false
+     */
+    private boolean profilePhotoIsSameAsImageFromPath(WebElement profilePhotoElement, String imagePath) {
         BufferedImage expectedImage = null;
         try {
-            expectedImage = ImageIO.read(new File("src/main/resources/static/img/default.jpg"));
+            expectedImage = ImageIO.read(new File(imagePath));
         } catch (IOException e) {
-            fail("Error loading default profile photo during test");
+            fail("Error loading expected image during test");
         }
         Screenshot logoImageScreenshot = new AShot().takeScreenshot(webDriver, profilePhotoElement);
         BufferedImage actualImage = logoImageScreenshot.getImage();
 
+        File actualTest = new File("src/test/resources/static/img/actualTest.jpg");
+        File expectedTest = new File("src/test/resources/static/img/expectedTest.jpg");
+
+        BufferedImage resizedImage = new BufferedImage(actualImage.getWidth(), actualImage.getHeight(), BufferedImage.SCALE_SMOOTH);
+        Graphics2D graphics2D = resizedImage.createGraphics();
+        graphics2D.drawImage(expectedImage, 0, 0, actualImage.getWidth(), actualImage.getHeight(), null);
+        graphics2D.dispose();
+
+
+        try {
+            boolean check1 = ImageIO.write(actualImage, "png", actualTest);
+            boolean check2 = ImageIO.write(resizedImage, "png", expectedTest);
+            System.err.println("Written" + check1 + check2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         ImageDiffer imgDiff = new ImageDiffer();
-        ImageDiff diff = imgDiff.makeDiff(actualImage, expectedImage);
-        assertTrue(diff.hasDiff(), "Images are Sames");
+        ImageDiff diff = imgDiff.makeDiff(actualImage, resizedImage).withDiffSizeTrigger((int) (actualImage.getHeight() * actualImage.getWidth() * 0.5));
+
+        System.err.println(diff.hasDiff());
+        System.err.println(actualImage.getHeight() * actualImage.getWidth());
+        return !diff.hasDiff();
+    }
+
+    @Then("My profile photo is the default photo")
+    public void myProfilePhotoIsTheDefaultPhoto() {
+        assertTrue(webDriver.findElement(By.id("uploadPreview")).isDisplayed());
+        assertTrue(profilePhotoIsSameAsImageFromPath(webDriver.findElement(By.id("uploadPreview")),
+                "src/main/resources/static/img/default.jpg"));
     }
 
     @Given("My profile photo is not the default photo")
     public void myProfilePhotoIsNotTheDefaultPhoto() {
-        try {
-            myProfilePhotoIsTheDefaultPhoto();
+//        try {
+////            myProfilePhotoIsTheDefaultPhoto();
+//            iUploadAProfilePhoto();
+//        } catch (AssertionFailedError e) {
+//            // Profile photo is not the default photo so step passes
+//        }
+        if (profilePhotoIsSameAsImageFromPath(webDriver.findElement(By.id("uploadPreview")),
+                "src/main/resources/static/img/default.jpg")) {
             iUploadAProfilePhoto();
-        } catch (AssertionFailedError e) {
-            // Profile photo is not the default photo so step passes
         }
     }
 
     @When("I upload a profile photo")
     public void iUploadAProfilePhoto() {
-        webDriver.findElement(By.id("avatar")).sendKeys(new File("src/test/resources/static/img/T100Logo.png").getAbsolutePath());
+        webDriver.findElement(By.id("avatar")).sendKeys(new File("src/test/resources/static/img/T100Logo.jpg").getAbsolutePath());
         wait.until(ExpectedConditions.elementToBeClickable(By.id("save-btn")));
         webDriver.findElement(By.id("save-btn")).click();
         wait.until(ExpectedConditions.elementToBeClickable(By.id("removeUpdateAlert")));
+        assertTrue(profilePhotoIsSameAsImageFromPath(webDriver.findElement(By.id("uploadPreview")),
+                "src/test/resources/static/img/T100Logo.jpg"));
     }
 
     @Then("My small version of my profile photo is displayed in the header of the page")
     public void mySmallVersionOfMyProfilePhotoIsDisplayedInTheHeaderOfThePage() {
         assertTrue(webDriver.findElement(By.id("userIconSmall")).isDisplayed());
-        WebElement profilePhotoElement = webDriver.findElement(By.id("userIconSmall"));
-        BufferedImage expectedImage = null;
-        try {
-            expectedImage = ImageIO.read(new File("src/main/resources/static/img/userImage.jpg"));
-        } catch (IOException e) {
-            fail("Error loading default profile photo during test");
-        }
-        Screenshot logoImageScreenshot = new AShot().takeScreenshot(webDriver, profilePhotoElement);
-        BufferedImage actualImage = logoImageScreenshot.getImage();
-
-        ImageDiffer imgDiff = new ImageDiffer();
-        ImageDiff diff = imgDiff.makeDiff(actualImage, expectedImage);
-        assertTrue(diff.hasDiff(), "Images are Sames");
+        assertTrue(profilePhotoIsSameAsImageFromPath(webDriver.findElement(By.id("userIconSmall")),
+                "src/main/resources/static/img/userImage.jpg"));
     }
 }
