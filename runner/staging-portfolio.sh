3dbf64550dc3e4b582ed93587064ab64df3768c0
Christopher Hamdajani <ckh35@uclive.ac.nz>
2022-05-06 20:36:17 +1200

    do some testing to deploy application in the vm #chore

--- runner/staging-portfolio.sh
+++ runner/staging-portfolio.sh
@@ -3,6 +3,7 @@ fuser -k 9501/tcp || true
 java -jar staging-portfolio/libs/portfolio-0.0.1-SNAPSHOT.jar \
     --server.port=9501 \
     --spring.application.name=portfolio \
+    --spring.profiles.active=test \
     --grpc.client.identity-provider-grpc-server.address=static://127.0.0.1:9500 \
     --grpc.client.identity-provider-grpc-server.enableKeepAlive=true \
     --grpc.client.identity-provider-grpc-server.keepAliveWithoutCalls=true \
