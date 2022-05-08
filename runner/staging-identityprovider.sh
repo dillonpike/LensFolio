b4fda2363b293e514bd91b4a729c1ce1c07f19cc
Christopher Hamdajani <ckh35@uclive.ac.nz>
2022-05-08 20:09:37 +1200

    Fix the properties file for test deployment so that it can access the mariadb #chore #fix

--- runner/staging-identityprovider.sh
+++ runner/staging-identityprovider.sh
@@ -1,3 +1,3 @@
 fuser -k 9500/tcp || true
-source /home/gitlab-runner/deploy.env
+
 java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar --spring.application.name=identity-provider --spring.profiles.active=test --grpc.server.port=9500
