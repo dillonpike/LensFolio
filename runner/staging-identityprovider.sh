d30c5d53462905ceaabe21f20e4fd142b95cfab4
Christopher Hamdajani <ckh35@uclive.ac.nz>
2022-05-08 22:30:33 +1200

    Fic name of a file int the deployment vm #chore #fix

--- runner/staging-identityprovider.sh
+++ runner/staging-identityprovider.sh
@@ -1,3 +1,3 @@
 fuser -k 9500/tcp || true
-source staging-portfolio/env.sh
+source staging-identityprovider/env.sh
 java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar --spring.application.name=identity-provider --spring.profiles.active=test --grpc.server.port=9500
