5a4bc486d140909a596de761921e95eb94673600
Dillon Pike <dkp33@uclive.ac.nz>
2022-05-09 18:24:18 +1200

    Added automated GUI test for checking the default start and end dates of a new sprint. #test

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/cucumber/selenium/ProjectDetailsStepDefs.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/cucumber/selenium/ProjectDetailsStepDefs.java
@@ -2,9 +2,10 @@ package nz.ac.canterbury.seng302.portfolio.cucumber.selenium;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.cucumber.java.en.Given;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import nz.ac.canterbury.seng302.portfolio.model.Project;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -12,19 +13,48 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Date;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * Selenium Cucumber step definitions for the project details feature.
+ */
 public class ProjectDetailsStepDefs {
 
+    /**
+     * Webdriver used during tests.
+     */
     private WebDriver webDriver;
+
+    /**
+     * WebDriverWait object that is used to wait until some criteria is met, for example an element to be visible.
+     */
     private WebDriverWait wait;
 
+    /**
+     * End date of the last sprint.
+     */
+    private Date lastSprintEndDate;
+
+    /**
+     * Sets up for scenario by getting a web driver and WebDriverWait object.
+     */
     @Before
     public void setUp() {
         webDriver = SeleniumService.getWebDriver();
         wait = SeleniumService.getWait();
+    }
+
+    /**
+     * Tears down after running scenario by quitting the web driver (thus closing the browser) and setting the web
+     * driver to null.
+     */
+    @After
+    public void tearDown() {
+        SeleniumService.tearDownWebDriver();
     }
 
     @Then("I can view a page with details of the project")
@@ -55,11 +85,62 @@ public class ProjectDetailsStepDefs {
         assertTrue(webDriver.findElement(By.className("project-desc")).getText().equalsIgnoreCase("test project desc"));
         //assertTrue(webDriver.findElement(By.className("sprint-block")).isDisplayed());
     }
-    
 
-    @After
-    public void tearDown() {
-        SeleniumService.tearDownWebDriver();
+    @When("I browse to the project page")
+    public void iBrowseToTheProjectPage() {
+        webDriver.findElement(By.id("projectsHeaderButton")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(., 'Project Description')]")));
     }
 
+    @And("I am on the project page")
+    public void iAmOnTheProjectPage() {
+        iBrowseToTheProjectPage();
+    }
+
+
+    @When("I browse to the add sprint page")
+    public void iBrowseToTheAddSprintPage() {
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("addSprintButton")));
+        webDriver.findElement(By.id("addSprintButton")).click();
+    }
+
+    @And("There is a sprint")
+    public void thereIsASprint() {
+        while (!webDriver.findElements(By.id("deleteSprintButton")).isEmpty()) {
+            webDriver.findElement(By.id("deleteSprintButton")).click();
+        }
+        iAddASprint();
+        String dateString = webDriver.findElement(By.className("sprint-date")).getText();
+        lastSprintEndDate = Project.stringToDate(dateString.substring(dateString.indexOf("-")+2));
+    }
+
+    @And("I add a sprint")
+    public void iAddASprint() {
+        webDriver.findElement(By.id("addSprintButton")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("saveButton")));
+        webDriver.findElement(By.id("saveButton")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(., 'Project Description')]")));
+    }
+
+    @Then("The start date should be one day after the end date of the previous sprint")
+    public void theStartDateShouldBeOneDayAfterTheEndDateOfThePreviousSprint() {
+        String sprintStartDate = webDriver.findElement(By.id("sprintStartDate")).getAttribute("value");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(lastSprintEndDate);
+        calendar.add(Calendar.DATE, 1);
+        String expectedStartDate = Project.dateToString(calendar.getTime());
+        assertEquals(expectedStartDate, sprintStartDate);
+    }
+
+    @And("The end date should be {int} weeks after the start date")
+    public void theEndDateShouldBeWeeksAfterTheStartDate(int numWeeks) {
+        String sprintStartDate = webDriver.findElement(By.id("sprintStartDate")).getAttribute("value");
+        String sprintEndDate = webDriver.findElement(By.id("sprintEndDate")).getAttribute("value");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(Project.stringToDate(sprintStartDate));
+        calendar.add(Calendar.WEEK_OF_MONTH, 3);
+        calendar.add(Calendar.DATE, -1);
+        String expectedEndDate = Project.dateToString(calendar.getTime());
+        assertEquals(expectedEndDate, sprintEndDate);
+    }
 }
--- portfolio/src/test/resources/features/ProjectDetails.feature
+++ portfolio/src/test/resources/features/ProjectDetails.feature
@@ -1,4 +1,4 @@
-Feature: U12 Project Details
+Feature: UPi Project Details
 
   Scenario: AC1 - As a user, I can browse to a page that contains the project details
     Given I am logged in as admin
@@ -7,6 +7,15 @@ Feature: U12 Project Details
 
   Scenario: AC2 - As a teacher, I can create and add all the details for a project.
   Appropriate validation is always applied. All changes are persistent.
-    Given I am logged in as "Teacher"
+    Given I am logged in as "teacher"
     When I browse to the project page
     Then I can create and add all details for a project
+
+  Scenario: AC7 - Default sprint start date is day after the previous sprint end.
+    Default sprint end date is 3 weeks after the default start date.
+    Given I am logged in as "teacher"
+    And I am on the project page
+    And There is a sprint
+    When I browse to the add sprint page
+    Then The start date should be one day after the end date of the previous sprint
+    And The end date should be 3 weeks after the start date
