feeff59ebc886775bf8eaecb28f574de380a79c5
Christopher Hamdajani <ckh35@uclive.ac.nz>
2022-05-31 15:57:14 +1200

    Create draft test for edit deadline controller test #test

---
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/controller/EditDeadlineControllerTest.java
@@ -0,0 +1,56 @@
+package nz.ac.canterbury.seng302.portfolio.controller;
+
+import nz.ac.canterbury.seng302.portfolio.service.DeadlinesService;
+import nz.ac.canterbury.seng302.portfolio.service.SprintService;
+import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import nz.ac.canterbury.seng302.shared.identityprovider.ClaimDTO;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = EditDeadlineController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class EditDeadlineControllerTest {
+
+    /**
+     * AuthState object to be used when we mock security context
+     */
+    public AuthState validAuthState = AuthState.newBuilder()
+            .setIsAuthenticated(true)
+            .setNameClaimType("name")
+            .setRoleClaimType("role")
+            .addClaims(ClaimDTO.newBuilder().setType("role").setValue("ADMIN").build()) // Set the mock user's role
+            .addClaims(ClaimDTO.newBuilder().setType("nameid").setValue("123456").build()) // Set the mock user's ID
+            .setAuthenticationType("AuthenticationTypes.Federation")
+            .setName("validtesttoken")
+            .build();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private DeadlinesService deadlinesService;
+
+    @Test
+    void eventEditSave() {
+        SecurityContext mockedSecurityContext = Mockito.mock(SecurityContext.class);
+        when(mockedSecurityContext.getAuthentication()).thenReturn(new PreAuthenticatedAuthenticationToken(validAuthState, ""));
+
+        SecurityContextHolder.setContext(mockedSecurityContext);
+
+
+    }
+}
