4292dec6fbb913402062610c4c167af13b1c6dc4
Dillon Pike <dkp33@uclive.ac.nz>
2022-06-01 15:04:59 +1200

    Added documentation for deadline model class. #document #pair[dkp33, wjo34]

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/Deadline.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/Deadline.java
@@ -6,57 +6,121 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Date;
 
+/**
+ * JPA entity that models a deadline of a project. Each deadline has a parent project that it's in,
+ * a name, a date (that includes a time).
+ */
 @Entity
 public class Deadline {
 
 
+    /**
+     * Id of the deadline.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    /**
+     * Id of the project the deadline is apart of.
+     */
     private int parentProjectId;
+
+    /**
+     * Name of the deadline.
+     */
     private String deadlineName;
+
+    /**
+     * Date of the deadline (includes time).
+     */
     private Date deadlineDate;
 
+    /**
+     * Gets the id of the deadline
+     * @return the id of the deadline
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Sets the id of the deadline
+     * @param id id of the deadline
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets the project id that the deadline is in
+     * @return project id that the deadline is in
+     */
     public int getParentProjectId() {
         return parentProjectId;
     }
 
+    /**
+     * Sets the project id that the deadline is in
+     * @param parentProjectId project id that the deadline is in
+     */
     public void setParentProjectId(int parentProjectId) {
         this.parentProjectId = parentProjectId;
     }
 
+    /**
+     * Gets the name of the deadline
+     * @return name of the deadline
+     */
     public String getDeadlineName() {
         return deadlineName;
     }
 
+    /**
+     * Sets the name of the deadline
+     * @param deadlineName name of the deadline
+     */
     public void setDeadlineName(String deadlineName) {
         this.deadlineName = deadlineName;
     }
 
+    /**
+     * Gets the date of the deadline
+     * @return date of the deadline
+     */
     public Date getDeadlineDate() {
         return deadlineDate;
     }
 
+    /**
+     * Sets the date of the deadline
+     * @param deadlineDate date of the deadline
+     */
     public void setDeadlineDate(Date deadlineDate) {
         this.deadlineDate = deadlineDate;
     }
 
+    /**
+     * Empty constructor for JPA.
+     */
     public Deadline() {}
 
+    /**
+     * Constructs a Deadline object.
+     * @param parentProjectId the parent project of the deadline
+     * @param deadlineName name of the deadline
+     * @param deadlineDate date of the deadline
+     */
     public Deadline(int parentProjectId, String deadlineName, Date deadlineDate) {
         this.parentProjectId = parentProjectId;
         this.deadlineName = deadlineName;
         this.deadlineDate = deadlineDate;
     }
 
+    /**
+     * String representation of the deadline object
+     * @return String representation of the deadline object
+     */
     @Override
     public String toString() {
         return String.format(
