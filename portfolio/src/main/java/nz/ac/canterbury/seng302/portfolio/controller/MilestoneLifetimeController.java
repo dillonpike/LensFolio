63fc768426968df787ae00a820600139c67088ae
Christopher Hamdajani <ckh35@uclive.ac.nz>
2022-06-08 23:02:51 +1200

    Build backend to add milestone and persist it to database which includes creating entity class,service class, controller class, repository interface #feature

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/MilestoneLifetimeController.java
@@ -0,0 +1,32 @@
+package nz.ac.canterbury.seng302.portfolio.controller;
+
+import nz.ac.canterbury.seng302.portfolio.model.Milestone;
+import nz.ac.canterbury.seng302.portfolio.service.MilestoneService;
+import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * Controller that handles adding and deleting milestone.
+ */
+@Controller
+public class MilestoneLifetimeController {
+    /**
+     * Service for persisting milestone.
+     */
+    @Autowired
+    private MilestoneService milestoneService;
+
+    /**
+     * Saves the given milestone to the database and redirects the user to the details page.
+     * @param milestone new milestone to be saved
+     */
+    @PostMapping("/add-deadline")
+    public String milestoneSave(@ModelAttribute("milestone") Milestone milestone, @AuthenticationPrincipal AuthState principal) {
+        milestoneService.addMilestone(milestone);
+        return "redirect:/details";
+    }
+}
