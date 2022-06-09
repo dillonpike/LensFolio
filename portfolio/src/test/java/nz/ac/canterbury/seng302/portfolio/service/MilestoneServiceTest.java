f74fc1c10eaca3952ed03ef5310536131de57417
Dillon Pike <dkp33@uclive.ac.nz>
2022-06-09 14:26:40 +1200

    Added unit tests for controller and service methods for deleting milestones #test

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/service/MilestoneServiceTest.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/service/MilestoneServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Date;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
@@ -46,5 +47,30 @@ class MilestoneServiceTest {
 
         verify(milestoneRepository, times(1)).save(expectedMilestone);
         assertEquals(expectedMilestone, milestone);
+    }
+
+    /**
+     * Tests that the testRemove method calls the repository's deleteById method with the expected milestone id when a
+     * milestone with the given id exists.
+     */
+    @Test
+    void testRemoveMilestoneExists() {
+        Milestone expectedMilestone = new Milestone(0,"Test Milestone", new Date());
+        when(milestoneRepository.findById(any(Integer.class))).thenReturn(Optional.of(expectedMilestone));
+        milestoneService.removeMilestone(expectedMilestone.getId());
+
+        verify(milestoneRepository, times(1)).deleteById(expectedMilestone.getId());
+    }
+
+    /**
+     * Tests that the testRemove method does not call the repository's deleteById method with when a milestone with the
+     * given id does not exist.
+     */
+    @Test
+    void testRemoveMilestoneDoesNotExist() {
+        when(milestoneRepository.findById(any(Integer.class))).thenReturn(Optional.empty());
+        milestoneService.removeMilestone(1);
+
+        verify(milestoneRepository, times(0)).deleteById(any(Integer.class));
     }
 }
