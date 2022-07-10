a92750b8d60aad2a99a797806134e86a90110838
Dillon Pike <dkp33@uclive.ac.nz>
2022-07-10 23:40:15 +1200

    Updated the delete modal to fill in its data with an event listener so it can be reused for events, milestones, and deadlines without adding extra modals to the page #refactor

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/MilestoneLifetimeController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/MilestoneLifetimeController.java
@@ -2,9 +2,7 @@ package nz.ac.canterbury.seng302.portfolio.controller;
 
 import nz.ac.canterbury.seng302.portfolio.model.Milestone;
 import nz.ac.canterbury.seng302.portfolio.service.MilestoneService;
-import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
@@ -34,7 +32,7 @@ public class MilestoneLifetimeController {
      * Tries to delete a milestone with given id.
      * @param id id of milestone being deleted
      */
-    @DeleteMapping("/delete-milestone/{id}")
+    @GetMapping("/delete-milestone/{id}")
     public String milestoneRemove(@PathVariable("id") Integer id) {
         milestoneService.removeMilestone(id);
         return "redirect:/details";
--- portfolio/src/main/resources/static/js/project.js
+++ portfolio/src/main/resources/static/js/project.js
@@ -7,3 +7,31 @@
 function checkDateKeys(event) {
     return '1234567890/'.includes(event.key) || [8, 13, 46].includes(event.keyCode);
 }
+
+/**
+ * Customises the delete modal attributes with depending on what type of thing (e.g. sprint, event, milestone) is
+ * being deleted, and which thing it is.
+ */
+function deleteModalSetup() {
+    const deleteModal = document.getElementById('deleteModal')
+    deleteModal.addEventListener('show.bs.modal', function (event) {
+        // Button that triggered the modal
+        const button = event.relatedTarget
+
+        // Extract info from data-bs-* attributes
+        const id = button.getAttribute('data-bs-id')
+        const name = button.getAttribute('data-bs-name')
+        const type = button.getAttribute('data-bs-type')
+
+        // Update the modal's content.
+        const modalTitle = deleteModal.querySelector('.modal-title')
+        const modalBodyLabel = deleteModal.querySelector('.modal-body label')
+        const modalButton = deleteModal.querySelector('.modal-footer button')
+        const modalLink = deleteModal.querySelector('.modal-footer a')
+
+        modalTitle.innerText = `Delete ${type.charAt(0).toUpperCase() + type.slice(1)}`
+        modalBodyLabel.textContent = `Are you sure you want to delete ${name}?`
+        modalButton.innerHTML = `Delete ${type.charAt(0).toUpperCase() + type.slice(1)}`
+        modalLink.href = `delete-${type}/${id}`
+    })
+}
