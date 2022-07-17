265a6c7f64f55abd2e93aff79732c3097ef577ed
Jamie Thomas <jth141@uclive.ac.nz>
2022-07-17 18:53:46 +1200

    Added logger for http response code errors. #fix

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/ErrorController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/ErrorController.java
@@ -1,9 +1,12 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
 
+import nz.ac.canterbury.seng302.portfolio.service.DateValidationService;
 import nz.ac.canterbury.seng302.portfolio.service.ElementService;
 import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -19,6 +22,8 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Controller
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ErrorController.class);
 
     @Autowired
     private UserAccountClientService userAccountClientService;
@@ -45,6 +50,7 @@ public class ErrorController implements org.springframework.boot.web.servlet.err
 
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         if (status != null) {
+            logger.debug(status.toString());
             Integer statusCode = Integer.valueOf(status.toString());
             if (statusCode == HttpStatus.FORBIDDEN.value()) {
                 return "redirect:login?forbidden";
