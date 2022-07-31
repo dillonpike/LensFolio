18ce38c1a4778a1d9d6d4e8781cbe6c42ad5159c
Christopher Hamdajani <ckh35@uclive.ac.nz>
2022-07-31 17:01:03 +1200

    update test for group service continued #test

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/service/GroupServiceTest.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/service/GroupServiceTest.java
@@ -1,23 +1,27 @@
 package nz.ac.canterbury.seng302.portfolio.service;
 
 import nz.ac.canterbury.seng302.portfolio.model.Group;
-import nz.ac.canterbury.seng302.shared.identityprovider.GroupsServiceGrpc;
-import nz.ac.canterbury.seng302.shared.identityprovider.UserAccountServiceGrpc;
+import nz.ac.canterbury.seng302.shared.identityprovider.*;
 import org.hibernate.ObjectNotFoundException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Unit tests for GroupService class.
@@ -26,7 +30,7 @@ import static org.mockito.Mockito.mock;
 class GroupServiceTest {
 
     /**
-     * The class that we want to test in this case UserAccountClientService Class
+     * The class that we want to test in this case GroupService Class
      */
     @Autowired
     private static GroupService groupService = new GroupService();
@@ -45,60 +49,136 @@ class GroupServiceTest {
         groupService.groupsServiceBlockingStub = groupsServiceBlockingStub;
     }
 
-//    /**
-//     * Given that there are no Group objects stored in the database, then the getGroupById method should throw an
-//     * ObjectNotFoundException.
-//     */
-//    @Test
-//    void givenNoStoredGroups_whenGetGroupById_thenThrowException() {
-//        given(groupRepository.findById(any(Integer.class))).willReturn(Optional.empty());
-//        int id = 1;
-//        String expectedFindExceptionMessage = "No row with the given identifier exists: [Group#" + id + "]";
-//        try {
-//            groupService.getGroupById(id);
-//            fail();
-//        } catch (ObjectNotFoundException exception) {
-//            assertEquals(expectedFindExceptionMessage, exception.getMessage());
-//        }
-//    }
-//
-//    /**
-//     * Given that there are Group objects stored in the database, then the getGroupById method should return the
-//     * expected Group object.
-//     */
-//    @Test
-//    void givenStoredGroups_whenGetGroupById_thenReturnExpectedGroup() {
-//        given(groupRepository.findById(any(Integer.class))).willReturn(Optional.of(expectedGroup));
-//        try {
-//            Group group = groupService.getGroupById(1);
-//            assertEquals(expectedGroup, group);
-//        } catch (Exception e) {
-//            fail();
-//        }
-//    }
-//
-//    /**
-//     * Tests that adding members to groups works as expected.
-//     */
-//    @Test
-//    void givenMemberNotInGroup_whenAddMember_thenMemberInGroup() {
-//        int expectedUserId = 1;
-//        Group group = new Group("", "", 1);
-//        assertFalse(group.getMemberIds().contains(expectedUserId));
-//        groupService.addMember(expectedUserId, group);
-//        assertTrue(group.getMemberIds().contains(expectedUserId));
-//    }
-//
-//    /**
-//     * Tests that removing members from groups works as expected.
-//     */
-//    @Test
-//    void givenMemberInGroup_whenRemoveMember_thenMemberNotInGroup() {
-//        int expectedUserId = 1;
-//        Group group = new Group("", "", 1);
-//        groupService.addMember(expectedUserId, group);
-//        assertTrue(group.getMemberIds().contains(expectedUserId));
-//        groupService.removeMember(expectedUserId, group);
-//        assertFalse(group.getMemberIds().contains(expectedUserId));
-//    }
+
+    /**
+     * Test to check create group method works in GroupService Class
+     * Expect that groupsServiceBlockingStub.createGroup() method to be called with the right content of the request
+     */
+    @Test
+    void callCreateGroupMethod_expectCreateGroupMethodTobeCalled() {
+        CreateGroupResponse response = CreateGroupResponse.newBuilder()
+                .setIsSuccess(true)
+                .build();
+        Mockito.when(groupsServiceBlockingStub.createGroup(any(CreateGroupRequest.class))).thenReturn(response);
+        ArgumentCaptor<CreateGroupRequest> captor = ArgumentCaptor.forClass(CreateGroupRequest.class);
+        groupService.createNewGroup("test short", "test long");
+        Mockito.verify(groupsServiceBlockingStub).createGroup(captor.capture());
+        assertEquals("test long", captor.getValue().getLongName());
+        assertEquals("test short", captor.getValue().getShortName());
+    }
+
+    /**
+     * Test to check method to add user(s) to a groups works in GroupService Class
+     * Expect that groupsServiceBlockingStub.addGroupMembers() method to be called with the right content of the request
+     */
+    @Test
+    void callAddMemberToGroup_expectAddGroupMembersTobeCalled() {
+        AddGroupMembersResponse response = AddGroupMembersResponse.newBuilder()
+                        .setIsSuccess(true).build();
+        Mockito.when(groupsServiceBlockingStub.addGroupMembers(any(AddGroupMembersRequest.class))).thenReturn(response);
+        ArgumentCaptor<AddGroupMembersRequest> captor = ArgumentCaptor.forClass(AddGroupMembersRequest.class);
+        Integer groupId = 1;
+        ArrayList<Integer> userIds = new ArrayList<Integer>();
+        userIds.add(1);
+        groupService.addMemberToGroup(groupId, userIds);
+        Mockito.verify(groupsServiceBlockingStub).addGroupMembers(captor.capture());
+        assertEquals(groupId, captor.getValue().getGroupId());
+        assertEquals(userIds, captor.getValue().getUserIdsList());
+    }
+
+    /**
+     * Test to check method to remove user(s) from a groups works in GroupService Class
+     * Expect that groupsServiceBlockingStub.removeGroupMembers() method to be called with the right content of the request
+     */
+    @Test
+    void callRemoveMembersFromGroup_expectRemoveMembersFromGroupTobeCalled() {
+        RemoveGroupMembersResponse response = RemoveGroupMembersResponse.newBuilder()
+                        .setIsSuccess(true).build();
+        Mockito.when(groupsServiceBlockingStub.removeGroupMembers(any(RemoveGroupMembersRequest.class))).thenReturn(response);
+        ArgumentCaptor<RemoveGroupMembersRequest> captor = ArgumentCaptor.forClass(RemoveGroupMembersRequest.class);
+        Integer groupId = 1;
+        ArrayList<Integer> userIds = new ArrayList<Integer>();
+        userIds.add(1);
+        userIds.add(2);
+        groupService.removeMembersFromGroup(groupId, userIds);
+        Mockito.verify(groupsServiceBlockingStub).removeGroupMembers(captor.capture());
+        assertEquals(groupId, captor.getValue().getGroupId());
+        assertEquals(userIds, captor.getValue().getUserIdsList());
+    }
+
+    /**
+     * Test to check method to modify group's details works in GroupService Class
+     * Expect that groupsServiceBlockingStub.modifyGroupDetails() method to be called with the right content of the request
+     */
+    @Test
+    void callEditGroupDetailsMethod_expectModifyGroupDetailsTobeCalled() {
+        ModifyGroupDetailsResponse response = ModifyGroupDetailsResponse.newBuilder()
+                        .setIsSuccess(true).build();
+        Mockito.when(groupsServiceBlockingStub.modifyGroupDetails(any(ModifyGroupDetailsRequest.class))).thenReturn(response);
+        ArgumentCaptor<ModifyGroupDetailsRequest> captor = ArgumentCaptor.forClass(ModifyGroupDetailsRequest.class);
+        Integer groupId = 1;
+        String shortName = "test short";
+        String longName = "test long";
+        groupService.editGroupDetails(groupId, shortName, longName);
+        Mockito.verify(groupsServiceBlockingStub).modifyGroupDetails(captor.capture());
+        assertEquals(groupId, captor.getValue().getGroupId());
+        assertEquals(shortName, captor.getValue().getShortName());
+        assertEquals(longName, captor.getValue().getLongName());
+    }
+
+    /**
+     * Test to check method to delete a group works in GroupService Class
+     * Expect that groupsServiceBlockingStub.deleteGroup() method to be called with the right content of the request
+     */
+    @Test
+    void callDeleteGroupMethod_expectDeleteGroupTobeCalled() {
+        DeleteGroupResponse response = DeleteGroupResponse.newBuilder()
+                        .setIsSuccess(true).build();
+        Mockito.when(groupsServiceBlockingStub.deleteGroup(any(DeleteGroupRequest.class))).thenReturn(response);
+        ArgumentCaptor<DeleteGroupRequest> captor = ArgumentCaptor.forClass(DeleteGroupRequest.class);
+        Integer groupId = 1;
+
+        groupService.deleteGroup(groupId);
+        Mockito.verify(groupsServiceBlockingStub).deleteGroup(captor.capture());
+        assertEquals(groupId, captor.getValue().getGroupId());
+    }
+
+    /**
+     * Test to check method to get a group details works in GroupService Class
+     * Expect that groupsServiceBlockingStub.getGroupDetails() method to be called with the right content of the request
+     */
+    @Test
+    void callGetGroupDetailsMethod_expectGetGroupDetailsTobeCalled() {
+        GroupDetailsResponse response = GroupDetailsResponse.newBuilder()
+                        .setGroupId(1)
+                        .build();
+
+        Mockito.when(groupsServiceBlockingStub.getGroupDetails(any(GetGroupDetailsRequest.class))).thenReturn(response);
+        ArgumentCaptor<GetGroupDetailsRequest> captor = ArgumentCaptor.forClass(GetGroupDetailsRequest.class);
+        Integer groupId = 1;
+        groupService.getGroupDetails(groupId);
+        Mockito.verify(groupsServiceBlockingStub).getGroupDetails(captor.capture());
+        assertEquals(groupId, captor.getValue().getGroupId());
+    }
+
+    /**
+     * Test to check method to get list of groups works in GroupService Class
+     * Expect that groupsServiceBlockingStub.getPaginatedGroups() method to be called with the right content of the request
+     */
+    @Test
+    void callGetPaginatedGroupsMethod_expectGetPaginatedGroupsTobeCalled() {
+        PaginatedGroupsResponse response = PaginatedGroupsResponse.newBuilder()
+                .build();
+
+        Mockito.when(groupsServiceBlockingStub.getPaginatedGroups(any(GetPaginatedGroupsRequest.class))).thenReturn(response);
+        ArgumentCaptor<GetPaginatedGroupsRequest> captor = ArgumentCaptor.forClass(GetPaginatedGroupsRequest.class);
+        Integer offset = 1;
+        String orderBy = "test";
+        boolean isAscending = true;
+        groupService.getPaginatedGroups(offset, 10, orderBy, isAscending);
+        Mockito.verify(groupsServiceBlockingStub).getPaginatedGroups(captor.capture());
+        assertEquals(offset, captor.getValue().getOffset());
+        assertEquals(isAscending, captor.getValue().getIsAscendingOrder());
+        assertEquals(orderBy, captor.getValue().getOrderBy());
+    }
 }
