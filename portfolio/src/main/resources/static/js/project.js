2ba7fbc6620e3a113dea435b3d0c640e007e8289
Christopher Hamdajani <ckh35@uclive.ac.nz>
2022-07-31 22:57:38 +1200

    Implement new date time picker for milestone #refactor #feature

--- portfolio/src/main/resources/static/js/project.js
+++ portfolio/src/main/resources/static/js/project.js
@@ -147,7 +147,7 @@ function milestoneModalSetup() {
 
         modalForm.setAttribute('object', milestone);
         modalBodyInput.value = milestone.milestoneName
-        $('#milestoneDateInput').datepicker('setDate', milestone.milestoneDateString)
+        deadlineDatePicker.dates.setValue(tempusDominus.DateTime.convert(new Date(milestone.milestoneDate)));
 
         // Initial run of validation functions in case initial values are invalid
         validateModalDate('milestoneDate', 'milestoneModalButton', 'milestoneDateAlertBanner', 'milestoneDateAlertMessage')
