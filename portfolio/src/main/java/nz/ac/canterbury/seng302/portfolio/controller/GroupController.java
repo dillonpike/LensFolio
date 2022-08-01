672c8472edbe0cba00453dc1579930802b4a3384
Haipeng Liu <hli165@uclive.ac.nz>
2022-08-01 19:04:03 +1200

    Implemented replace(update) table when click different groups #feature

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupController.java
@@ -9,6 +9,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Controller for group page
@@ -33,4 +35,12 @@ public class GroupController {
         elementService.addHeaderAttributes(model, id);
         return "group";
     }
+
+    @RequestMapping("/groups/local")
+    public String localRefresh(Model model) {
+        System.out.println("1");
+        model.addAttribute("title", "Group1");
+        return "group::table_refresh";
+    }
+
 }
