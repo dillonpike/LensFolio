31d65bc6ef0a2a80fd7613fa0be883fcd9f12800
John Elliott <jel119@uclive.ac.nz>
2022-08-01 21:37:24 +1200

    Completed the mocking test for the GroupModelServerService. #test

--- identityprovider/src/test/java/nz/ac/canterbury/seng302/identityprovider/service/GroupModelServerServiceTest.java
+++ identityprovider/src/test/java/nz/ac/canterbury/seng302/identityprovider/service/GroupModelServerServiceTest.java
@@ -1,23 +1,19 @@
 package nz.ac.canterbury.seng302.identityprovider.service;
 
 import io.grpc.stub.StreamObserver;
-import nz.ac.canterbury.seng302.identityprovider.model.Roles;
-import nz.ac.canterbury.seng302.identityprovider.model.UserModel;
-import nz.ac.canterbury.seng302.identityprovider.repository.RolesRepository;
 import nz.ac.canterbury.seng302.shared.identityprovider.*;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
+/**
+ * Sets up tests for the group model server service used to delete object from the repository when given a request through GRPC.
+ * This is done through mocking and stubbing.
+ */
 class GroupModelServerServiceTest {
 
     @Mock
@@ -26,18 +22,38 @@ class GroupModelServerServiceTest {
     @InjectMocks
     private GroupModelServerService groupModelServerService = Mockito.spy(GroupModelServerService.class);
 
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this); // This is required for Mockito annotations to work
+    }
+
+    /**
+     * Tests the ability given a valid group ID is sent in the request through GRPC from the portfolio.
+     * That it will successfully delete the group from the repository.
+     */
     @Test
     void testDeleteExistingGroup() {
+        // Stubs the request and observer.
         DeleteGroupRequest request = DeleteGroupRequest.newBuilder().setGroupId(1).build();
+        StreamObserver<DeleteGroupResponse> observer = Mockito.mock(StreamObserver.class);
 
+        // Setups up mock.
         when(groupModelService.removeGroup(anyInt())).thenReturn(true);
 
+        // Runs tasks for deleting existing group.
+        groupModelServerService.deleteGroup(request, observer);
 
-        UserRoleChangeResponse reply = groupModelServerService.deleteGroup(request);
-        Assertions.assertEquals(true,reply.getIsSuccess());
-        Mockito.verify(mockUserModel).deleteRole(deletedRole.capture());
-        Mockito.verify(userModelService).saveEditedUser(deletedRoleModel.capture());
+        // Checks it ran .onCompleted().
+        verify(observer, times(1)).onCompleted();
+        // Sets up captures to get value of responses.
+        ArgumentCaptor<DeleteGroupResponse> captor = ArgumentCaptor.forClass(DeleteGroupResponse.class);
+        // Checks it ran .onNext() and gets the response.
+        verify(observer, times(1)).onNext(captor.capture());
+        // Gets the value of the response from the captor.
+        DeleteGroupResponse response = captor.getValue();
 
+        // Checks that the group was removed.
+        assertTrue(response.getIsSuccess());
     }
 
 }
