3f24e7da0a595c5a45ed0b917318b02c5432bd47
John Elliott <jel119@uclive.ac.nz>
2022-08-01 17:32:23 +1200

    Fixed the logic of saving a photo on dev. Also, error that occurred if the IDP image was deleted. #fix

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/PhotoService.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/PhotoService.java
@@ -2,7 +2,6 @@ package nz.ac.canterbury.seng302.portfolio.service;
 
 
 import nz.ac.canterbury.seng302.portfolio.PortfolioApplication;
-import nz.ac.canterbury.seng302.portfolio.utility.Utility;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +9,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import static nz.ac.canterbury.seng302.portfolio.utility.Utility.getApplicationLocation;
 
@@ -21,16 +22,24 @@ public class PhotoService {
     @Value("${spring.datasource.url}")
     private String dataSource;
 
+    /**
+     * Gets the users photo path based off the provided image name through profileImagePath and user through userId.
+     * Depending on the system running (dev,test,prod) different paths may be returned.
+     *
+     * @param profileImagePath  IDP file pathway.
+     * @param userId            The users ID.
+     * @return The path to use to display this image.
+     */
     public String getPhotoPath(String profileImagePath, int userId) {
         String[] paths;
         String path;
-        if (profileImagePath.equals("")) {
+        if (profileImagePath.equals("")) { // Default image.
             return "images/default.jpg";
-        } else if (getApplicationLocation(dataSource).equals("dev")) {
+        } else if (getApplicationLocation(dataSource).equals("dev")) { // Save image locally as dev is running.
             savePhotoToPortfolio(profileImagePath, userId);
             paths = profileImagePath.split("/identityprovider");
             path = paths[1];
-        } else {
+        } else { // The VM uses a shared folder currently so no additional saving is necessary.
             paths = profileImagePath.split("/" + getApplicationLocation(dataSource));
             path = "/" + getApplicationLocation(dataSource) + paths[1];
         }
@@ -40,30 +49,33 @@ public class PhotoService {
 
     /**
      * Saves the photo from the given path to the portfolio module for loading into the HTML.
-     * @param photoPath path of the photo
+     * This only occurs on the Dev system as on the VM the folder is accessible from both modals.
+     *
+     * @param photoPath Path of the photo
+     * @param userId    The user's ID.
      */
     public void savePhotoToPortfolio(String photoPath, int userId) {
         try {
             File imageFile;
             String directory = PortfolioApplication.IMAGE_DIR+ "/" + getApplicationLocation(dataSource) + "/" + userId + "/public/";
             new File(directory).mkdirs();
-            if (!photoPath.equals("")) {
-                String[] paths = photoPath.split("/identityprovider");
-                String path = "/portfolio" + paths[1];
-                imageFile = new File(photoPath);
-                if (imageFile.length() == 0) {
-                    imageFile = new File(PortfolioApplication.IMAGE_DIR + "/default.jpg");
+
+            imageFile = new File(photoPath); // The photo in the IDP
+            if (imageFile.length() == 0) { // If no photo exists use the default image.
+                URL resource = getClass().getClassLoader().getResource("static/images/default.jpg");
+                if (resource == null) {
+                    throw new NullPointerException("No default image.");
                 }
-            } else {
-                imageFile = new File(PortfolioApplication.IMAGE_DIR + "/default.jpg");
+                imageFile = new File(resource.toURI());
             }
-            File usedImageFile = new File(directory + "profileImage");
+
+            File usedImageFile = new File(directory + "profileImage"); // Saves locally for use in HTML.
             FileOutputStream imageOutput = new FileOutputStream(usedImageFile);
             FileInputStream imageInput = new FileInputStream(imageFile);
             imageOutput.write(imageInput.readAllBytes());
             imageInput.close();
             imageOutput.close();
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException | NullPointerException e) {
             e.printStackTrace();
         }
     }
