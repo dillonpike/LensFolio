6391cd789fbe79040333403ec5ce39921cd04e74
Dillon Pike <dkp33@uclive.ac.nz>
2022-08-03 13:12:14 +1200

    Added invalid case test for the GRPC method for deleting a group #test #pair[dkp33, jel119]
    Signed-off-by: Dillon Pike <dkp33@uclive.ac.nz>

--- identityprovider/src/test/java/nz/ac/canterbury/seng302/identityprovider/service/GroupModelServerServiceTest.java
+++ identityprovider/src/test/java/nz/ac/canterbury/seng302/identityprovider/service/GroupModelServerServiceTest.java
@@ -4,7 +4,9 @@ import io.grpc.stub.StreamObserver;
 import nz.ac.canterbury.seng302.shared.identityprovider.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -14,6 +16,7 @@ import static org.mockito.Mockito.*;
  * Sets up tests for the group model server service used to delete object from the repository when given a request through GRPC.
  * This is done through mocking and stubbing.
  */
+@ExtendWith(MockitoExtension.class)
 class GroupModelServerServiceTest {
 
     @Mock
@@ -25,14 +28,9 @@ class GroupModelServerServiceTest {
     @InjectMocks
     private GroupModelServerService groupModelServerService = Mockito.spy(GroupModelServerService.class);
 
-    @BeforeEach
-    public void init() {
-        MockitoAnnotations.openMocks(this); // This is required for Mockito annotations to work
-    }
-
     /**
-     * Tests the ability given a valid group ID is sent in the request through GRPC from the portfolio.
-     * That it will successfully delete the group from the repository.
+     * Tests the ability given a valid group ID is sent in the request through GRPC from the portfolio,
+     * that it will successfully delete the group from the repository.
      */
     @Test
     void testDeleteExistingGroup() {
@@ -56,6 +54,34 @@ class GroupModelServerServiceTest {
 
         // Checks that the group was removed.
         assertTrue(response.getIsSuccess());
+    }
+
+    /**
+     * Tests the ability given an invalid group ID is sent in the request through GRPC from the portfolio,
+     * that it will unsuccessfully delete the group from the repository.
+     */
+    @Test
+    void testDeleteNonExistingGroup() {
+        // Build the request.
+        DeleteGroupRequest request = DeleteGroupRequest.newBuilder().setGroupId(1).build();
+
+        // Setups up mock outcomes.
+        when(groupModelService.removeGroup(anyInt())).thenReturn(false);
+
+        // Runs tasks for deleting existing group.
+        groupModelServerService.deleteGroup(request, observer);
+
+        // Checks it ran .onCompleted().
+        verify(observer, times(1)).onCompleted();
+        // Sets up captures to get the response.
+        ArgumentCaptor<DeleteGroupResponse> captor = ArgumentCaptor.forClass(DeleteGroupResponse.class);
+        // Checks it ran .onNext() and captor the response.
+        verify(observer, times(1)).onNext(captor.capture());
+        // Gets the value of the response from the captor.
+        DeleteGroupResponse response = captor.getValue();
+
+        // Checks that the group was removed.
+        assertFalse(response.getIsSuccess());
     }
 
 }
