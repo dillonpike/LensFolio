c41e12366549cf261568d15f18d725efba9af8b6
Haipeng Liu <hli165@uclive.ac.nz>
2022-08-08 17:12:36 +1200

    Implemented feature to connect Gitlab repo #feature #pair[hli165, jel119]

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupSettingController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupSettingController.java
@@ -5,6 +5,9 @@ import nz.ac.canterbury.seng302.portfolio.service.ElementService;
 import nz.ac.canterbury.seng302.portfolio.service.GroupService;
 import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import org.gitlab4j.api.GitLabApi;
+import org.gitlab4j.api.GitLabApiException;
+import org.gitlab4j.api.models.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -12,6 +15,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 
 /**
@@ -32,10 +37,13 @@ public class GroupSettingController {
     @GetMapping("/groupSetting")
     public String groupSetting(@RequestParam(value = "groupId") int groupId,
                                @AuthenticationPrincipal AuthState principal,
-                               Model model) {
+                               Model model) throws GitLabApiException {
         Integer id = userAccountClientService.getUserIDFromAuthState(principal);
         elementService.addHeaderAttributes(model, id);
 
+        GitLabApi gitLabApi = new GitLabApi("https://eng-git.canterbury.ac.nz", "XPY6WX5wpc6iaxiyxCjN");
+        Project project = gitLabApi.getProjectApi().getProject("12361");
+        System.out.println(gitLabApi.getCommitsApi().getCommits("12361"));
         groupService.addGroupDetailToModel(model, groupId);
         return "groupSetting";
     }
---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/GitlabApiService.java
@@ -0,0 +1,7 @@
+package nz.ac.canterbury.seng302.portfolio.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GitlabApiService {
+}
