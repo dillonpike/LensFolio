84d6242575f5709efb427023b06ad87c9e231efe
Dillon Pike <dkp33@uclive.ac.nz>
2022-08-15 21:45:28 +1200

    When receiving a notification of an update to the project details page, the page now waits to refresh until any currently open modals are closed. #reengineer

--- portfolio/src/main/resources/static/js/project.js
+++ portfolio/src/main/resources/static/js/project.js
@@ -13,6 +13,22 @@ function checkTimeKeys(event) {
 }
 
 /**
+ * Returns true if a modal is open, otherwise false.
+ * @returns {boolean} true if a modal is open, otherwise false
+ */
+function isModalOpen() {
+    return document.getElementsByClassName('modal fade show').length > 0
+}
+
+/**
+ * Returns the modal that is currently open
+ * @returns {Element} modal that is currently open
+ */
+function getOpenModal() {
+    return document.getElementsByClassName('modal fade show')[0]
+}
+
+/**
  * Customises the sprint modal attributes with depending on what sprint it should display and whether it's being used
  * for adding or editing a sprint.
  */
@@ -258,51 +274,4 @@ function eventModalSetup() {
         updateCharsLeft('eventName', 'eventNameLength', 50)
         $('#' + modalButton.getAttribute("id")).prop('hidden', false);
     })
-    /**
-     * Customises the sprint modal attributes with depending on what sprint it should display and whether it's being used
-     * for adding or editing a sprint.
-     */
-    function sprintModalSetup() {
-        const sprintModal = document.getElementById('sprintModal')
-        sprintModal.addEventListener('show.bs.modal', function (event) {
-            // Button that triggered the modal
-            const button = event.relatedTarget
-
-            // Extract info from data-bs-* attributes
-            const sprint = JSON.parse(button.getAttribute('data-bs-sprint'))
-            const type = button.getAttribute('data-bs-type')
-
-            // Update the modal's content.
-            const modalTitle = sprintModal.querySelector('.modal-title')
-            const modalBodyInputs = sprintModal.querySelectorAll('.modal-body input')
-            const modalBodyTextArea = sprintModal.querySelector('.modal-body textarea')
-            const modalButton = sprintModal.querySelector('.modal-footer button')
-            const modalForm = sprintModal.querySelector('form')
-
-            if (type === 'add') {
-                modalTitle.innerText = 'Add Sprint'
-                modalButton.innerHTML = 'Add Sprint'
-                modalForm.action = 'add-sprint'
-                modalForm.setAttribute('data-sprint-id', -1)
-            } else {
-                modalTitle.innerText = 'Edit Sprint'
-                modalButton.innerHTML = 'Save Sprint'
-                modalForm.action = `edit-sprint/${sprint.id}`
-                modalForm.setAttribute('data-sprint-id', sprint.id)
-            }
-
-            modalBodyInputs[0].value = sprint.name
-            $('#sprintStart').datepicker('setDate', sprint.startDateString)
-            $('#sprintEnd').datepicker('setDate', sprint.endDateString)
-            modalBodyTextArea.value = sprint.description
-
-            // Initial run of updateSprintDateError function in case initial values are invalid
-            updateSprintDateError();
-            updateCharsLeft('sprintName', 'sprintNameLength', 50);
-            updateCharsLeft('sprintDescription', 'sprintDescriptionLength', 500);
-        })
-        $("#sprintModalButton").on('click', function (ignore) {
-            pageReload();
-        });
-    }
 }
