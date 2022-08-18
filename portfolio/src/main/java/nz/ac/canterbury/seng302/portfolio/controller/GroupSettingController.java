be996f2d59022c6e542fa145a0eb52c848c17d73
Christopher Hamdajani <ckh35@uclive.ac.nz>
2022-08-18 19:19:06 +1200

    Created methods for getting branch names, members, and commits that can be filtered by branch and user from the GitLab API. #feature #pair[dkp33, ckh35]
    Signed-off-by: Dillon Pike <dkp33@uclive.ac.nz>

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupSettingController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupSettingController.java
@@ -1,22 +1,14 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
 
-import nz.ac.canterbury.seng302.portfolio.service.ElementService;
-import nz.ac.canterbury.seng302.portfolio.service.GroupService;
-import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
+import nz.ac.canterbury.seng302.portfolio.service.*;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
-import org.gitlab4j.api.GitLabApi;
-import org.gitlab4j.api.GitLabApiException;
-import org.gitlab4j.api.models.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 
 /**
@@ -32,18 +24,18 @@ public class GroupSettingController {
     private UserAccountClientService userAccountClientService;
 
     @Autowired
-    public GroupService groupService;
+    private GroupService groupService;
+
+    @Autowired
+    private GitLabApiService gitLabApiService;
 
     @GetMapping("/groupSetting")
     public String groupSetting(@RequestParam(value = "groupId") int groupId,
                                @AuthenticationPrincipal AuthState principal,
-                               Model model) throws GitLabApiException {
+                               Model model) {
         Integer id = userAccountClientService.getUserIDFromAuthState(principal);
         elementService.addHeaderAttributes(model, id);
 
-        GitLabApi gitLabApi = new GitLabApi("https://eng-git.canterbury.ac.nz", "XPY6WX5wpc6iaxiyxCjN");
-        Project project = gitLabApi.getProjectApi().getProject("12361");
-        System.out.println(gitLabApi.getCommitsApi().getCommits("12361"));
         groupService.addGroupDetailToModel(model, groupId);
         return "groupSetting";
     }
