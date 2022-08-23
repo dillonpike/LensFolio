91cab1ceecfb379b4a7684b552ccd82ac950bc6b
Haipeng Liu <hli165@uclive.ac.nz>
2022-08-23 16:29:47 +1200

    Add input validate check for all input fields #feature

--- portfolio/src/main/resources/static/js/groupSetting.js
+++ portfolio/src/main/resources/static/js/groupSetting.js
@@ -12,3 +12,46 @@ function updateCommitsList() {
 }
 
 updateCommitsList()
+
+
+/**
+ * Each time a character is typed/pasted will be checked uses a regex validator that are not part of a valid set,
+ * replace invalid character with a blank character
+ */
+function inputValidateCheck() {
+    const longNameText = document.getElementById('longGroupName');
+    const repoName = document.getElementById('repoName');
+    const repoID = document.getElementById('repoID');
+    const repoToken = document.getElementById('repoToken');
+
+    longNameText.addEventListener( "input", event => {
+        longNameText.value = longNameText.value.replace( /[^a-zA-Z0-9~!@#$%^&*()_+|}{:"?><,./;' ]/gm, '');
+    }, false);
+    longNameText.addEventListener( "paste", event => {
+        longNameText.value = longNameText.value.replace( /[^a-zA-Z0-9~!@#$%^&*()_+|}{:"?><,./;' ]/gm, '');
+    }, false);
+
+    repoName.addEventListener( "input", event => {
+        repoName.value = repoName.value.replace( /[^a-zA-Z0-9~!@#$%^&*()_+|}{:"?><,./;' ]/gm, '');
+    }, false);
+    repoName.addEventListener( "paste", event => {
+        repoName.value = repoName.value.replace( /[^a-zA-Z0-9~!@#$%^&*()_+|}{:"?><,./;' ]/gm, '');
+    }, false);
+
+    // Only allow numbers
+    repoID.addEventListener( "input", event => {
+        repoID.value = repoID.value.replace( /[^0-9]/gm, '');
+    }, false);
+    repoID.addEventListener( "paste", event => {
+        repoID.value = repoID.value.replace( /[^0-9]/gm, '');
+    }, false);
+
+    repoToken.addEventListener( "input", event => {
+        repoToken.value = repoToken.value.replace( /[^a-zA-Z0-9~!@#$%^&*()_+|}{:"?><,./;' ]/gm, '');
+    }, false);
+    repoToken.addEventListener( "paste", event => {
+        repoToken.value = repoToken.value.replace( /[^a-zA-Z0-9~!@#$%^&*()_+|}{:"?><,./;' ]/gm, '');
+    }, false);
+
+
+}
