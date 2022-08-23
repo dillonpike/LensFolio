21f389d8c7ed2a8d26488fa5c4d02e63eb595c58
Jamie Thomas <jth141@uclive.ac.nz>
2022-08-23 15:03:36 +1200

    Made controller method. Still unable to test as the real save functionality needs to be built first. #feature

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupSettingController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupSettingController.java
@@ -1,9 +1,12 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
 
+import nz.ac.canterbury.seng302.portfolio.model.NotificationGroup;
 import nz.ac.canterbury.seng302.portfolio.service.*;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -39,5 +42,17 @@ public class GroupSettingController {
         groupService.addGroupDetailToModel(model, groupId);
         return "groupSetting";
     }
+
+    /**
+     * Websocket controller to send notification to users to have their pages refreshed when new settings are saved.
+     * @param notificationGroup Holds the ID of the group being refreshed.
+     * @return The notificationGroup object.
+     */
+    @MessageMapping("/save-group-settings")
+    @SendTo("/webSocketGet/group-settings-saved")
+    public NotificationGroup refreshGroupSettings(NotificationGroup notificationGroup) {
+        return notificationGroup;
+    }
+
 }
 
