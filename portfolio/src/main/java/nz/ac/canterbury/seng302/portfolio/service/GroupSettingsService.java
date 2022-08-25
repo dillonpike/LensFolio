401676a249f477b83c2986fc4c20d1f718af3ab9
Haipeng Liu <hli165@uclive.ac.nz>
2022-08-25 18:55:58 +1200

    update

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/GroupSettingsService.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/GroupSettingsService.java
@@ -7,8 +7,6 @@ import org.hibernate.ObjectNotFoundException;
 import org.jvnet.hk2.annotations.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.mariadb.jdbc.internal.logging.Logger;
-import org.mariadb.jdbc.internal.logging.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ui.Model;
