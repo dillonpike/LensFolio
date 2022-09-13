b3c71e193f5492f91e6a3855c952dac8e5d5b945
Jamie Thomas <jth141@uclive.ac.nz>
2022-09-13 15:01:03 +1200

    Fixed arrow icons getting out of sync with the collapse parts they were associated with. #fix

--- portfolio/src/main/resources/static/js/collapse.js
+++ portfolio/src/main/resources/static/js/collapse.js
@@ -9,21 +9,35 @@ function changeIcon(arrowId) {
     let iconHtml = document.getElementById(arrowId.getAttribute('id'))
     if (iconHtml.getAttribute("class").includes("left")) {
         sessionStorage.setItem(icon.attr('id'), "false");
-        iconHtml.getElementsByTagName("path").item(0).setAttribute('d', "m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z");
     } else {
         sessionStorage.setItem(icon.attr('id'), "true");
-        iconHtml.getElementsByTagName("path").item(0).setAttribute('d', "M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z");
     }
+
 }
 
 /**
  * Toggles the collapse of the element.
  * @param collapseButtonId  The id of the button that toggles the collapse.
  * @param arrowId  The id of the arrow to toggle.
+ * @param collapseId Id of the element to collapse, to add the animation event listeners to flip the arrows.
  */
-function setToggle(collapseButtonId, arrowId) {
+function setToggle(collapseButtonId, arrowId, collapseId) {
+    let collapse = document.getElementById(collapseId);
+    collapse.addEventListener("show.bs.collapse", function (event) {
+        let svgArray = Array.from(event.target.parentElement.getElementsByTagName("svg"));
+        let arrow = svgArray.find(svg => svg.id.includes("Arrow"));
+        let iconHtml = document.getElementById(arrow.getAttribute('id'));
+        iconHtml.getElementsByTagName("path").item(0).setAttribute('d', "M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z");
+    });
+    collapse.addEventListener("hide.bs.collapse", function (event) {
+        let svgArray = Array.from(event.target.parentElement.getElementsByTagName("svg"));
+        let arrow = svgArray.find(svg => svg.id.includes("Arrow"));
+        let iconHtml = document.getElementById(arrow.getAttribute('id'));
+        iconHtml.getElementsByTagName("path").item(0).setAttribute('d', "m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z");
+    });
     let shouldToggle = sessionStorage.getItem(arrowId);
     if (shouldToggle === "true") {
         document.getElementById(collapseButtonId).click();
     }
 }
+
