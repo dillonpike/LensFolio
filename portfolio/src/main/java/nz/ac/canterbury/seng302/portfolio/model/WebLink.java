f505af2bd310ad8b8a71ed9f286f54c0c13351e0
John Elliott <jel119@uclive.ac.nz>
2022-09-14 13:51:51 +1200

    Added style to the weblink URLs being displayed in evidence. This also adds icons to show if the weblink is secure or not. #feature #pair[jel119, jth141]

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/WebLink.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/WebLink.java
@@ -44,4 +44,32 @@ public class WebLink {
         this.url = url;
     }
 
+    /**
+     * Creates a URL title that can be displayed to the user, rather than the full URL .
+     * @return The URL to be displayed.
+     */
+    public String getFormattedUrl() {
+        String stringUrl = this.url;
+        String[] urlContent = stringUrl.split("://");
+
+        if (urlContent.length < 2) {
+            // When this is return to the HTMl page through Thymeleaf the link will not be displayed.
+            stringUrl = "InvalidURL";
+        } else {
+            stringUrl = urlContent[1];
+        }
+
+        if (stringUrl.length() > 50) {
+            stringUrl = stringUrl.substring(0, 50) + "...";
+        }
+        return stringUrl;
+    }
+
+    /**
+     * Returns if the URL has an HTTPS start. Meaning it is secure.
+     * @return If the URL is secure.
+     */
+    public boolean isSecure() {
+        return this.url.startsWith("https");
+    }
 }
