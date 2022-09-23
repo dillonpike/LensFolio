a95fa15ff47a735f6926eff86ee8e277928cda1a
Dillon Pike <dkp33@uclive.ac.nz>
2022-09-23 14:29:44 +1200

    Added leaderboard basic leaderboard page with every user and placeholder scores. #feature #pair[dkp33, hli165]

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/LeaderboardController.java
@@ -0,0 +1,31 @@
+package nz.ac.canterbury.seng302.portfolio.controller;
+
+import nz.ac.canterbury.seng302.portfolio.service.ElementService;
+import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
+import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LeaderboardController {
+
+    @Autowired
+    private UserAccountClientService userAccountClientService;
+
+    @Autowired
+    private ElementService elementService;
+
+    @GetMapping("/leaderboard")
+    public String showLeaderboardPage(
+            Model model,
+            @AuthenticationPrincipal AuthState principal
+    ) {
+        Integer id = userAccountClientService.getUserIDFromAuthState(principal);
+        elementService.addHeaderAttributes(model, id);
+        model.addAttribute("users", userAccountClientService.getAllUsers().getUsersList());
+        return "leaderboard";
+    }
+}
