c84773a4f84667fc99cdb9e883008030d9f44fc1
John Elliott <jel119@uclive.ac.nz>
2022-09-27 23:34:30 +1300

    Added controller testing for the evidence skill page and its partial refreshing system. #test

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/controller/EvidenceControllerTest.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/controller/EvidenceControllerTest.java
@@ -2,12 +2,10 @@ package nz.ac.canterbury.seng302.portfolio.controller;
 
 import com.google.protobuf.Timestamp;
 import nz.ac.canterbury.seng302.portfolio.model.Evidence;
+import nz.ac.canterbury.seng302.portfolio.model.Tag;
 import nz.ac.canterbury.seng302.portfolio.model.WebLink;
 import nz.ac.canterbury.seng302.portfolio.service.*;
-import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
-import nz.ac.canterbury.seng302.shared.identityprovider.ClaimDTO;
-import nz.ac.canterbury.seng302.shared.identityprovider.UserResponse;
-import nz.ac.canterbury.seng302.shared.identityprovider.UserRole;
+import nz.ac.canterbury.seng302.shared.identityprovider.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,15 +22,17 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import static nz.ac.canterbury.seng302.portfolio.controller.EvidenceController.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Tests for the evidence controller.
@@ -369,5 +369,105 @@ class EvidenceControllerTest {
                 .andExpect(model().attribute(ADD_EVIDENCE_MODAL_FRAGMENT_TITLE_MESSAGE, "Title must not contain emojis"))
                 .andExpect(model().attribute(ADD_EVIDENCE_MODAL_FRAGMENT_DESCRIPTION_MESSAGE, "Description must not contain emojis"));
         verify(evidenceService, times(0)).addEvidence(any(Evidence.class));
+    }
+
+
+    /**
+     * Tests that the evidence skills page is able to be reached when a valid data is given (Blue Sky Scenario).
+     * @throws Exception If mocking the MVC fails.
+     */
+    @Test
+    void testViewEvidenceSkillsPage200() throws Exception {
+        int userId = 1;
+        ArrayList<Evidence> evidences = new ArrayList<>();
+        evidences.add(new Evidence(0, userId, "test", "test-desc", Date.from(Instant.now())));
+        Tag tag = new Tag("Test");
+        when(evidenceService.getEvidencesWithSkill(any(Integer.class))).thenReturn(evidences);
+        when(tagService.getTag(any(Integer.class))).thenReturn(tag);
+
+        mockMvc.perform(get("/evidence-skills?userId=" + userId + "&skillId=1"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("evidencesExists", true))
+                .andExpect(model().attribute("evidences", evidences))
+                .andExpect(model().attribute("skillTag", tag))
+                .andExpect(model().attribute("viewedUserId", userId));
+
+        verify(evidenceService, times(1)).getEvidencesWithSkill(any(Integer.class));
+        verify(tagService, times(1)).getTag(any(Integer.class));
+    }
+
+    /**
+     * Tests that the evidence skills page will redirect a user to their account page when an invalid tag is given.
+     * @throws Exception If mocking the MVC fails.
+     */
+    @Test
+    void testViewEvidenceSkillsPageRedirectToAccountWhenSkillInvalid300() throws Exception {
+        int userId = 1; // Same as the userAccountClientService.getUserIDFromAuthState id.
+        ArrayList<Evidence> evidences = new ArrayList<>();
+        evidences.add(new Evidence(0, userId, "test", "test-desc", Date.from(Instant.now())));
+        when(evidenceService.getEvidencesWithSkill(any(Integer.class))).thenReturn(evidences);
+        when(tagService.getTag(any(Integer.class))).thenReturn(null);
+
+        mockMvc.perform(get("/evidence-skills?userId=" + userId + "&skillId=1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("account?userId=" + userId));
+
+        verify(evidenceService, times(1)).getEvidencesWithSkill(any(Integer.class));
+        verify(tagService, times(1)).getTag(any(Integer.class));
+    }
+
+    /**
+     * Tests that the evidence skills partial refresh is able to be reached when a valid data is given (Blue Sky Scenario).
+     * This also tests that when given the correct values all data will be returned to the page.
+     * @throws Exception If mocking the MVC fails.
+     */
+    @Test
+    void testViewEvidenceSkillsUpdateToViewAll200() throws Exception {
+        int userId = 1;
+        ArrayList<Evidence> evidences = new ArrayList<>();
+        evidences.add(new Evidence(0, userId, "test", "test-desc", Date.from(Instant.now())));
+        when(evidenceService.getEvidencesWithSkill(any(Integer.class))).thenReturn(evidences);
+
+        mockMvc.perform(get("/switch-evidence-list?userId=1&viewedUserId=" + userId + "&listAll=true&skillId=1"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("evidencesExists", true))
+                .andExpect(model().attribute("evidences", evidences));
+        verify(evidenceService, times(1)).getEvidencesWithSkill(any(Integer.class));
+    }
+
+    /**
+     * Tests that the evidence skills partial refresh is able to be reached when a valid data is given (Blue Sky Scenario).
+     * This also tests that when given the correct values only data matching to a users ID will be returned to the page.
+     * @throws Exception If mocking the MVC fails.
+     */
+    @Test
+    void testViewEvidenceSkillsUpdateToViewSingleUser200() throws Exception {
+        int userId = 1;
+        ArrayList<Evidence> evidences = new ArrayList<>();
+        evidences.add( new Evidence(0, userId, "test", "test-desc", Date.from(Instant.now())));
+        when(evidenceService.getEvidencesWithSkillAndUser(any(Integer.class), any(Integer.class))).thenReturn(evidences);
+
+        mockMvc.perform(get("/switch-evidence-list?userId=1&viewedUserId=" + userId + "&listAll=false&skillId=1"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("evidencesExists", true))
+                .andExpect(model().attribute("evidences", evidences));
+
+        verify(evidenceService, times(1)).getEvidencesWithSkillAndUser(any(Integer.class), any(Integer.class));
+    }
+
+    /**
+     * Tests that the evidence skills partial refresh will redirect a user to their account page when an invalid tag is given.
+     * @throws Exception If mocking the MVC fails.
+     */
+    @Test
+    void testViewEvidenceSkillsUpdateRedirectToAccountWhenSkillInvalid300() throws Exception {
+        int userId = 1;
+        when(evidenceService.getEvidencesWithSkill(any(Integer.class))).thenThrow(new NullPointerException("Invalid skill tag."));
+
+        mockMvc.perform(get("/switch-evidence-list?userId=" + userId + "&viewedUserId=" + userId + "&listAll=true&skillId=1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("account?userId=" + userId));
+
+        verify(evidenceService, times(1)).getEvidencesWithSkill(any(Integer.class));
     }
 }
