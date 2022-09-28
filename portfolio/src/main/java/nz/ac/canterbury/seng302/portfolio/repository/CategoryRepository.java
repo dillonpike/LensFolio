dd90f6d8942ddbbb1827107fb923c5e0629461d1
John Elliott <jel119@uclive.ac.nz>
2022-09-28 15:22:40 +1300

    Adds JPA database generation for the categories which also are linked to evidence through @ElementCollections. #feature #pair[rlh89,jel119]

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package nz.ac.canterbury.seng302.portfolio.repository;
+
+import nz.ac.canterbury.seng302.portfolio.model.Category;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryRepository extends CrudRepository<Category, Integer> {
+    Optional<Category> findById(int id);
+    List<Category> findAll();
+}
